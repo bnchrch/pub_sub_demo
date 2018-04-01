ea422b647a216863f85c8dbbec9ed284d1caeca7
Ben Church <ben.church@metalab.co>
2018-04-01 17:45:36 +0800

    Add HTTPpoison

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule PubSubDemo.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:httpoison, "~> 1.0"},
     ]
   end
 
