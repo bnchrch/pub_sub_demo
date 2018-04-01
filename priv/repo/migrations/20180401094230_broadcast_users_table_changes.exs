e85566d6b68bb8d79811d1d9b152051b2af7bebe
Ben Church <ben.church@metalab.co>
2018-04-01 19:16:02 +0800

    Update trigger to be compatible with update

--- priv/repo/migrations/20180401094230_broadcast_users_table_changes.exs
+++ priv/repo/migrations/20180401094230_broadcast_users_table_changes.exs
@@ -2,6 +2,7 @@ defmodule PubSubDemo.Repo.Migrations.BroadcastUsersTableChanges do
   use Ecto.Migration
 
   def up do
+    # Create a function that broadcasts row changes
     execute "
       CREATE OR REPLACE FUNCTION broadcast_changes()
       RETURNS trigger AS $$
@@ -13,19 +14,24 @@ defmodule PubSubDemo.Repo.Migrations.BroadcastUsersTableChanges do
         ELSE
           current_row := OLD;
         END IF;
+        IF (TG_OP = 'INSERT') THEN
+          OLD := NEW;
+        END IF;
       PERFORM pg_notify(
           'users_changes',
           json_build_object(
             'table', TG_TABLE_NAME,
             'type', TG_OP,
             'id', current_row.id,
-            'data', row_to_json(current_row)
+            'new_row_data', row_to_json(NEW),
+            'old_row_data', row_to_json(OLD)
           )::text
         );
       RETURN current_row;
       END;
       $$ LANGUAGE plpgsql;"
 
+    # Create a trigger links the users table to the broadcast function
     execute "
       CREATE TRIGGER notify_user_changes_trigger
       AFTER INSERT OR UPDATE OR DELETE
